0b628a479f37559b64ca572b4b3e17ea58795ed8
Prof. Dr. Ulrich Anders <prof.anders@online.de>
2025-04-23 17:33:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import "@mantine/core/styles.css";
-import { MantineProvider, Container, Slider, RangeSlider } from "@mantine/core";
+import {
+  MantineProvider,
+  Container,
+  Slider,
+  RangeSlider,
+  Text,
+  Kbd,
+} from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Container>
-        <Slider my={96} color="green" />
+        <Text c="green" size="xl" mt={96}>
+          Keyboard controls work:
+        </Text>
+        <Slider mb={96} color="green" />
+        <Text c="red" size="xl">
+          Keyboard controls do not work (RangeSlider not grabbing focus?):
+        </Text>
         <RangeSlider color="red" />
+        <Text c="red" size="xl">
+          Suggestion: <Kbd>{"←"}</Kbd> and <Kbd>{"→"}</Kbd> for left thumb,
+          <Kbd>{"↑"}</Kbd> and <Kbd>{"↓"}</Kbd> for right thumb.
+        </Text>
       </Container>
     </MantineProvider>
   );
